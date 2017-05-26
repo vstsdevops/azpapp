1b1f0dcdbb04b036835c8d01e5bb03ac2ae33265
luckygiri <gp2713@gmail.com>
2017-05-26 09:46:48 +0530

    Update startInstall.ps1

--- dev/QTP/startInstall.ps1
+++ dev/QTP/startInstall.ps1
@@ -1,24 +1,29 @@
-﻿# Create temp directory
-New-Item "C:\temp\zipfile" -ItemType Directory -ErrorAction SilentlyContinue
+$NewDIR = "C:\SoftwaresDump\QTP12.5"
+
+$SoftwareWebLOC = "http://ftp.mphasism4l.cloud/Artifacts/QTP12.5/QTP%2012%20-%20HP%20UFT%2012.54.zip"
+
+$AutoITEXEWebLoc = "http://ftp.mphasism4l.cloud/Artifacts/QTP12.5/qtpsetup.exe"
+
+$SoftwarePath = "C:\SoftwaresDump\QTP12.5\QTP 12 - HP UFT 12.54.zip"
+
+
+# Create temp directory
+New-Item "$NewDIR" -ItemType Directory -ErrorAction SilentlyContinue
 
 # Download
-(New-Object System.Net.WebClient).DownloadFile("http://ftp.mphasism4l.cloud/Artifacts/QTP%2012%20-%20HP%20UFT%2012.54.zip", "C:\temp\zipfile\QTP 12 - HP UFT 12.54.zip")
-#Invoke-WebRequest "http://ftp.mphasism4l.cloud/Artifacts/QTP%2012%20-%20HP%20UFT%2012.54.zip" -OutFile "C:\temp\zipfile\QTP 12 - HP UFT 12.54.zip" 
+(New-Object System.Net.WebClient).DownloadFile("$SoftwareWebLOC", "$SOftwarePath")
+
 
 #Extracting
 
 $shell = New-Object -ComObject shell.application
-$zip = $shell.NameSpace("C:\temp\zipfile\QTP 12 - HP UFT 12.54.zip")
+$zip = $shell.NameSpace("$SOftwarePath")
 foreach ($item in $zip.items()) {
-  $shell.Namespace("C:\temp").CopyHere($item)
+  $shell.Namespace("$NewDIR").CopyHere($item)
 }
 
-(New-Object System.Net.WebClient).DownloadFile("http://ftp.mphasism4l.cloud/Artifacts/qtpsetup.exe", "C:\temp\qtpsetup.exe")
-#Invoke-WebRequest "http://ftp.mphasism4l.cloud/Artifacts/qtpsetup.exe" -OutFile "C:\temp\qtpsetup.exe" 
-
-
+(New-Object System.Net.WebClient).DownloadFile("$AutoITEXEWebLoc", "$NewDIR")
 
 # Install
-Start-Process "C:\temp\setup.exe"
-Start-Process "C:\temp\qtpsetup.exe"
-
+Start-Process "$NewDIR\setup.exe"
+Start-Process "$NewDIR\qtpsetup.exe"
