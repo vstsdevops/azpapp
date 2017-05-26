cd2e98ff97d5782e9a3cae0ba5eebff883d438e4
luckygiri <gp2713@gmail.com>
2017-05-26 14:37:44 +0530

    Update startInstall.ps1

--- dev/QTP/startInstall.ps1
+++ dev/QTP/startInstall.ps1
@@ -1,20 +1,13 @@
 $NewDIR = "C:\SoftwaresDump\QTP12.5"
-
 $SoftwareWebLink = "http://artifacts.g7crm4l.org/Artifacts/QTP12.5/QTP%2012%20-%20HP%20UFT%2012.54.zip"
-
-
 $SoftwarePath = "C:\SoftwaresDump\QTP12.5\QTP 12 - HP UFT 12.54.zip"
 
-
 # Create temp directory
-New-Item "$NewDIR" -ItemType Directory -ErrorAction SilentlyContinue
-
+New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -ErrorAction SilentlyContinue
 # Download
 (New-Object System.Net.WebClient).DownloadFile("$SoftwareWebLink", "$SoftwarePath")
 
-
 #Extracting
-
 $shell = New-Object -ComObject shell.application
 $zip = $shell.NameSpace("$SOftwarePath")
 foreach ($item in $zip.items()) {
@@ -26,5 +19,5 @@ foreach ($item in $zip.items()) {
 
 
 # Install
-Start-Process "$NewDIR\setup.exe"
-Start-Process "$NewDIR\qtpsetup.exe"
+Start-Process "C:\SoftwaresDump\QTP12.5\setup.exe"
+Start-Process "C:\SoftwaresDump\QTP12.5\qtpsetup.exe" 
