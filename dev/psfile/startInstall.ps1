ce9013952b0cc7ca96a23d80b3d5bed3a97a4ade
luckygiri <gp2713@gmail.com>
2017-05-26 18:08:58 +0530

    Update startInstall.ps1

--- dev/psfile/startInstall.ps1
+++ dev/psfile/startInstall.ps1
@@ -1,5 +1,5 @@
 
 
 # Install
-Start-Process "C:\SoftwaresDump\QTP12.5\setup.exe" 
-Start-Process "C:\SoftwaresDump\QTP12.5\qtpsetup.exe" -ArgumentList "/S /v""/qn""" 
+Start-Process "C:\SoftwaresDump\QTP12.5\setup.exe" -ArgumentList "/S /v""/qn""" -Wait
+Start-Process "C:\SoftwaresDump\QTP12.5\qtpsetup.exe"  -ArgumentList "/S /v""/qn""" -Wait
