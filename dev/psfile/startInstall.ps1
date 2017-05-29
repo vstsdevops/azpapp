57f8c23de6e96d0dbd0fd0d0ebf248c70db40564
luckygiri <gp2713@gmail.com>
2017-05-29 10:19:32 +0530

    Delete startInstall.ps1

--- dev/psfile/startInstall.ps1
+++
@@ -1,5 +0,0 @@
-
-
-# Install
-Start-Process "C:\SoftwaresDump\QTP12.5\setup.exe" -ArgumentList "/S /v""/qn""" -Wait
-Start-Process "C:\SoftwaresDump\QTP12.5\qtpsetup.exe"  -ArgumentList "/S /v""/qn""" -Wait
