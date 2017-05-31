69c775aed952076ad657436378c2e0c7054f9e60
luckygiri <gp2713@gmail.com>
2017-05-31 15:27:30 +0530

    Update startInstall.ps1

--- dev/QTP/startInstall.ps1
+++ dev/QTP/startInstall.ps1
@@ -22,7 +22,7 @@ foreach ($item in $zip.items()) {
 
 Start-Process "C:\SoftwaresDump\QTP12.5\setup.exe" -ArgumentList -Wait
 Start-Sleep -Seconds 30
-Start-Process "C:\SoftwaresDump\QTP12.5\qtpsetup.exe" -ArgumentList "/S /v""/qn""" 
+Start-Process "C:\SoftwaresDump\QTP12.5\qtpsetup.exe" -ArgumentList "/v""/qn""" 
 
 
 
