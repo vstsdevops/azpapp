87ecf340ef367b63eb3ed60c69af83cea1f6c5a9
vstsdevops <vstsdevops@outlook.com>
2018-09-26 00:35:03 +0530

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for tsdfffhe application.
+        /// </summary>ffffffsdfdf
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
